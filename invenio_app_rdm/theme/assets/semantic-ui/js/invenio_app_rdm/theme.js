57d89bbf1e9eaacf84a02062c7533179141020e1
Jenny Bonsak <jenny.bonsak@gmail.com>
2022-03-21 16:39:26 +0100

    header login: add a11y to profile menu

--- invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/theme.js
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/theme.js
@@ -1,3 +1,4 @@
+import jquery from 'jquery';
 
 /* Expand and collapse navbar  */
 const toggle = document.querySelector(".menu-icon");
@@ -12,3 +13,22 @@ function toggleMenu() {
   }
 }
 toggle && toggle.addEventListener("click", toggleMenu, false);
+
+
+/* User profile dropdown */
+jquery('#user-profile-dropdown.ui.dropdown')
+  .dropdown({
+    showOnFocus: false,
+    selectOnKeydown: false,
+    action: (text, value, element) => {
+      // needed to trigger navigation on keyboard interaction
+      let path = element.attr('href');
+      window.location.pathname=path;
+    },
+    onShow: () => {
+      jquery('#user-profile-dropdown-btn').attr('aria-expanded', true)
+    },
+    onHide: () => {
+      jquery('#user-profile-dropdown-btn').attr('aria-expanded', false)
+    }
+  });
